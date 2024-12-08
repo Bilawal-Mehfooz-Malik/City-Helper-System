20a526588fbf6814dfe24fd26ce1d0748ecc8a8b
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2024-12-08 14:13:02 +0500

    added user location repo test

--- app/test/src/mocks.dart
+++ app/test/src/mocks.dart
@@ -1,10 +1,17 @@
-import 'package:app/src/features/startup/data/location_repository.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:mocktail/mocktail.dart';
-
-class MockLocationRepository extends Mock implements LocationRepository {}
+import 'package:sembast/sembast.dart';
 
 class MockGeoLocator extends Mock implements GeolocatorPlatform {}
+
+class MockDatabase extends Mock implements Database {}
+
+class MockStoreRef extends Mock implements StoreRef<Object?, Object?> {}
+
+class MockRecordRef extends Mock implements RecordRef<Object?, Object?> {}
+
+class MockRecordSnapshot extends Mock
+    implements RecordSnapshot<String, Object?> {}
 
 class Listener<T> extends Mock {
   void call(T? previous, T next);
